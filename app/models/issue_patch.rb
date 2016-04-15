ec6bb8a9b9c84cdfe1262aac273bbbf3ecb1a4a3
Farkwun <bryan.chu@ryerson.ca>
2016-04-15 17:07:25 -0400

    Completely changing functionality entirely -
    Plugin now preserves existing Redmine per-status workflows (read-only
    fields for a specific status-role combination are not visible in the
    edit menu when not changing Status), but adds an aggregated workflow
    during the transition.
    
    If transitioning from a Status in which a custom field is
    optional/required to a Status in which the same custom field is
    read-only, the plugin changes the workflow rule for that custom field
    (during the transition *only*) to 'Required'.

--- app/models/issue_patch.rb
+++ app/models/issue_patch.rb
@@ -3,19 +3,50 @@ module IssuePatch
 		base.send(:include, InstanceMethods)
 
 		# Wrap the methods we are extending
-		base.alias_method_chain :safe_attribute_names, :removed_closed_item_fields
+		base.alias_method_chain :workflow_rule_by_attribute, :aggregate_workflows
 
 		# Exectue this code at the class level (not instance level)
 	end
 
 	module InstanceMethods
 			
-		def safe_attribute_names_with_removed_closed_item_fields(user=nil)
-			if status_was.is_closed? && !reopened?
-				'status_id' 
-			else
-				safe_attribute_names_without_removed_closed_item_fields(user)
-			end
+	  	# aggregates/ORs workflows so that switching to a status with  
+	 	# read_only fields (that should be filled once in a read-only status)
+	  	# changes them to required **during the change only** 
+		def workflow_rule_by_attribute_with_aggregate_workflows(user=nil)
+
+		 # change the status_id to the old status so as not to have to 
+		 # change the innards fo the workflow_rule_by_attribute method.
+		  
+		 # Storing result_was first is because the @workflow_rule_by_attribute
+		 # instance variable is set in the without method - in order to avoid
+		 # having to set the instance variable at the end of this method, 
+		 # having the order in this way sets the instance variable as originally
+		 # intended
+		 status_temp = self.status_id
+		 self.status_id = status_id_was
+
+		 # store the result
+	     result_was = workflow_rule_by_attribute_without_aggregate_workflows(user)
+
+		 # switch back to the current status
+		 self.status_id = status_temp
+		 result_is = workflow_rule_by_attribute_without_aggregate_workflows(user)
+
+		 result = result_is
+		 # normal behaviour if the two hashes are the same (in other words, 
+		 # the issue is not undergoing a meaningful status transition) 
+		 unless result_is == result_was
+		   # for every value that is read_only in the current status 
+		   # but is not read_only in the previous one,
+		   # set to required instead
+		   result_is.each do |k, v|
+			 if v == 'readonly' && result_was[k] != 'readonly'
+			   result[k] = 'required'
+			 end
+		   end
+		 end
+		 result
 		end
 	end
 end
